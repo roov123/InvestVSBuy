a72bc3a6579e2aabe795b03e1416bd396a645b31
roov123 <37275701+roov123@users.noreply.github.com>
2025-03-07 15:57:37 +1100

    Update PropVsInvest.py

--- PropVsInvest.py
+++ PropVsInvest.py
@@ -91,7 +91,10 @@ investible_assets=assets-property_budget
 #--------------------------------------------------------------------------------------------------------
 st.sidebar.subheader("🏡 Costs of buying property")
 # Sidebar for Property Costs
-st.sidebar.markdown("""<div style='border: 2px solid #007BFF; background-color:#007BFF; color:white; padding:10px; border-radius:5px; font-weight:bold; text-align:left;'>Upfront Costs</div>""", unsafe_allow_html=True)
+st.sidebar.header("Cost of Property")
+st.sidebar.markdown("""<div style='border: 2px solid #007BFF; background-color:#007BFF; color:white; padding:10px; border-radius:5px; font-weight:bold; text-align:center;'>Upfront Costs</div>""", unsafe_allow_html=True)
+
+st.sidebar.markdown("""<div style='border: 2px solid #007BFF; padding:10px; border-radius:5px;'>""", unsafe_allow_html=True)
 st.sidebar.write("Enter the estimated cost for each expense:")
 
 stamp_duty = st.sidebar.number_input("Stamp Duty ($)", value=20000, step=1000, min_value=0)
